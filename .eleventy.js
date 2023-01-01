c0b85fb23b5577592b51e504bde5cced121b06da
Jamie Shiell <jamie@infernus.org>
2023-01-01 22:15:19 +0000

    Change CNAME to use action rather than CNAME file

--- .eleventy.js
+++ .eleventy.js
@@ -12,7 +12,6 @@ module.exports = function(eleventyConfig) {
   // Copy the `img` and `css` folders to the output
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("css");
-  eleventyConfig.addPassthroughCopy("CNAME");
 
   // Add plugins
   eleventyConfig.addPlugin(pluginRss);
